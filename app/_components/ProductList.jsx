b25e41831bf6023e4d276e4fd5c5341ddccb4c7e
unknown <soroushnassaj@gmail.com>
2025-05-02 02:45:08 +0430

    products

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Producitem from './Producitem'
 
 function ProductList({productList}) {
+
+  const sortedProducts = [...productList].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+  const productListo = sortedProducts.slice(0, 8);
+
   return (
     <div className='mt-10' >
         <h2 className='font-bold text-green-600 text-2xl text-right '></h2>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-5 lg:gap-5 gap-1   ' >
-            {productList.map((product,index)=>index<8&&(
+            {productListo.map((product,index)=>index<8&&(
                 <Producitem key={index} product={product}/>
             ))}
         </div>
