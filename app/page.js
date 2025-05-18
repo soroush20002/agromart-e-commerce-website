6e2adbb18af3276cd14599a51cfed813a71450e0
unknown <soroushnassaj@gmail.com>
2025-05-18 17:30:35 +0430

    r3

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default async function Home() {
   const productList=await GlobalApi.getAllProducts();
 
   return (
-    <div className="pl-2 md:pl-10 pr-2 md:pr-10 pt-0 lg:pt-9 md:pt-0  ">
+    <div className="pl-2 md:pl-10 pr-2 md:pr-10 pt-0 lg:pt-9 md:pt-0 overflow-x-hidden w-full max-w-[100vw]">
       <Slider sliderList={sliderList} />
       <CategoryList categoryList={categoryList} />
       <ProductList productList={productList} />
